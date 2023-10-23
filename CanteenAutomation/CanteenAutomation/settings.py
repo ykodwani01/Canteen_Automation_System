3dc9d005cae6e49c063d4146d42e4fe247e03fde
Yash Kodwani <202101418@daiict.ac.in>
2023-10-23 16:39:33 +0530

    Connected Backend and Frontend

--- CanteenAutomation/CanteenAutomation/settings.py
+++ CanteenAutomation/CanteenAutomation/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'CanteenAutomation.urls'
@@ -122,7 +124,7 @@ MEDIA_URL='/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+CORS_ORIGIN_ALLOW_ALL = True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
--- CanteenAutomation/frontEnd/src/signIn_signUp/sign_up.js
+++ CanteenAutomation/frontEnd/src/signIn_signUp/sign_up.js
@@ -15,10 +15,50 @@ import { NavLink } from 'react-router-dom';
 //importing custom cmp
 import theme from '../general_compo/theme.js'
 import useReSize from './mediaQuery';
+import { useEffect, useState } from "react";
 
 function SignUp() {
   //custom hook for media query 
   const [background_style_ext,signIn_style_ext] = useReSize()
+  
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [confirmPassword,setConfirmPassword] = useState('')
+  const [buttonClicked,setButtonClicked] = useState(false)
+
+  const handleChange = (event) =>{
+    if(event.target.id==="Email"){
+      setEmail(event.target.value)
+    }
+    else if (event.target.id==="Password"){
+      setPassword(event.target.value)
+    }
+    else if(event.target.id==="Confirm Password"){
+      setConfirmPassword(event.target.value)
+    }
+  };
+  const apiUrl = 'http://localhost:8000/register';
+  useEffect(()=>{
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: `${email}`, password: `${password}` }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Data sent successfully:', data);
+      })
+      .catch((error) => {
+        console.error('Error sending data:', error);
+      });
+  },[buttonClicked])
+
+  const handleClick = ()=>{
+    setButtonClicked(!buttonClicked)
+  };
+
   return (
     <ThemeProvider theme={theme}>
       {/* background */}
@@ -26,10 +66,10 @@ function SignUp() {
         {/* sign-up box */}
         <Container className='signIn' sx={{background:"rgba(222,216,216,0.5)",borderRadius:'30px',...signIn_style_ext}}> 
           <Typography sx={{fontWeight:'bolder',fontSize:'30px',marginTop:'30px'}}>Sign Up</Typography>
-          <TextField id="outlined-basic" label="Email" variant="outlined" sx={{background:"rgba(250,249,246,0.1)",borderRadius:"5px",marginBottom:'30px',marginTop:'23px'}}/>
-          <TextField id="outlined-basic" label="Password" variant="outlined" sx={{background:"rgba(250,249,246,0.1)",borderRadius:"5px",marginBottom:'30px'}}/>
-          <TextField id="outlined-basic" label="Confirm Password" variant="outlined" sx={{background:"rgba(250,249,246,0.1)",borderRadius:"5px",marginBottom:'30px'}}/>
-          <Button variant="contained" sx={{fontWeight:"bolder",width:'220px',height:'50px',fontSize:'20px',textTransform:'none',marginBottom:'20px'}} disableElevation>Sign Up</Button>
+          <TextField id="Email" label="Email" value={email} onChange={handleChange} variant="outlined" sx={{background:"rgba(250,249,246,0.1)",borderRadius:"5px",marginBottom:'30px',marginTop:'23px'}}/>
+          <TextField id="Password" label="Password" value={password} onChange={handleChange}  variant="outlined" sx={{background:"rgba(250,249,246,0.1)",borderRadius:"5px",marginBottom:'30px'}}/>
+          <TextField id="Confirm Password" label="Confirm Password" value={confirmPassword} onChange={handleChange} variant="outlined" sx={{background:"rgba(250,249,246,0.1)",borderRadius:"5px",marginBottom:'30px'}}/>
+          <Button variant="contained" onClick={handleClick} sx={{fontWeight:"bolder",width:'220px',height:'50px',fontSize:'20px',textTransform:'none',marginBottom:'20px'}} disableElevation>Sign Up</Button>
           <NavLink to='/' style={{textDecoration:'none',color:'black'}}>
             <Container sx={{display:'flex',justifyContent:'center',marginBottom:'20px'}}>
               <Typography>Already have an account?</Typography><Typography color='primary'> Sign in</Typography>
