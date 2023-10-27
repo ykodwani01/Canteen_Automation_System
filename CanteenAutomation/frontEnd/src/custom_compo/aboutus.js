337577b196684cc68c5639eca94252d895741683
VrajPatel1810 <202101060@daiict.ac.in>
2023-10-27 19:35:44 +0530

    Added about us and contact us page

--- CanteenAutomation/frontEnd/src/custom_compo/aboutus.js
+++ CanteenAutomation/frontEnd/src/custom_compo/aboutus.js
@@ -1,8 +1,161 @@
-function AboutUs(){
-    return(
-        <div>
-            Lorem ipsum
-        </div>
+//importing css
+import '../App.css';
+
+//importing MUI cmp
+
+import { Typography, createTheme } from '@mui/material/';
+import { ThemeProvider } from '@mui/material/';
+import { green } from '@mui/material/colors';
+import Button from '@mui/material/Button';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+
+//importing photos
+import logo from '../general_compo/logo.png'
+import cafe from "../general_compo/cafe.png"
+import grp_img from "../general_compo/grp_img.jpg";
+
+
+//importing custom cmp
+import CartContent from './cartContent.js';
+import AccountContent from './accountContent.js';
+
+//defining theme
+const theme = createTheme({
+    palette: {
+        primary: { main: "#C31E2C" },
+        secondary: green
+    }
+})
+
+function AboutUs() {
+
+    //state for drawer
+    const [state, setState] = React.useState({
+        right: false,
+        left:false
+    });
+
+    //function for toggling the drawer
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (
+            event &&
+            event.type === 'keydown' &&
+            (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const closeButton = (anchor,status)=>{
+        setState({ ...state, [anchor]: status });
+    }
+
+    const drawerButton = (anchor,status)=>{
+        closeButton(anchor,status)
+    }
+
+    //list of all the items we need to display in the cart drawer
+    const list = (anchor) => (
+        <Box
+            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+            role="presentation"
+            //onClick={toggleDrawer(anchor, false)}
+            //onKeyDown={toggleDrawer(anchor, false)}
+        >
+            {anchor==="right"?<CartContent drawerButton={drawerButton} anchor={anchor}/>:<AccountContent drawerButton={drawerButton} anchor={anchor}/>}
+        </Box>
+    );
+
+    return (
+        <ThemeProvider theme={theme}>
+            {/* background */}
+            <div style={{ backgroundColor: '#DED8D8', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                {/* first box */}
+                <div style={{ borderRadius: '108px', marginTop: '70px', backgroundColor: '#EBE7E6', border: '2px solid white', width: '1341px', height: '732px', boxShadow: '0px 10px 5px darkgrey', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                    {/* padding box */}
+                    <div style={{ width: '1191px', height: '2032px' }}>
+                        {/* header div / Navigation bar */}
+                        <div style={{ display: 'flex', height: '70px', justifyContent: 'center', marginTop: '70px' }}>
+                            <img src={logo} alt='website logo' style={{ marginRight: '250px', height: '80px' }} />
+                            <div style={{ display: 'flex', boxShadow: '0px 2px 0px darkGrey', paddingBottom: '10px', marginTop: '10px' }}>
+                                <Button style={{ color: 'black', marginRight: '20px', marginTop: '10px', fontWeight: 'bold' }} href='/home'>Home</Button>
+                                <Button style={{ color: 'black', marginRight: '20px', marginTop: '10px', fontWeight: 'bold' }} href='/home/feedback'>Feedback</Button>
+                                <Button style={{ color: 'black', marginRight: '20px', marginTop: '10px', fontWeight: 'bold' }} href='/home/aboutus'>About Us</Button>
+                                <Button style={{ color: 'black', marginRight: '60px', marginTop: '10px', fontWeight: 'bold' }} href='/home/contact'>Contact</Button>
+                                
+{/* drawer for cart */}
+{['left'].map((anchor) => (
+                                    <React.Fragment key={anchor}>
+                                        <Button variant='contained' style={{ borderRadius: '30px', marginRight: '20px', marginTop: '10px', fontWeight: 'bold' }} onClick={toggleDrawer(anchor,true)}>Account</Button>
+                                        <SwipeableDrawer
+                                            anchor={anchor}
+                                            open={state[anchor]}
+                                            onClose={toggleDrawer(anchor, false)}
+                                            onOpen={toggleDrawer(anchor, true)}
+                                            PaperProps={{style:{borderTopRightRadius:'30px',backgroundColor: '#DED8D8',padding:'20px',width:'480px'}}}
+                                        >
+                                            {list(anchor)}
+                                        </SwipeableDrawer>
+                                    </React.Fragment>
+                                ))}
+                                {['right'].map((anchor) => (
+                                    <React.Fragment key={anchor}>
+                                        <Button variant='contained' startIcon={<ShoppingCartIcon />} style={{ borderRadius: '50px', marginRight: '20px', marginTop: '10px', fontWeight: 'bold' }} onClick={toggleDrawer(anchor, true)}>0</Button>
+                                        <SwipeableDrawer
+                                            anchor={anchor}
+                                            open={state[anchor]}
+                                            onClose={toggleDrawer(anchor, false)}
+                                            onOpen={toggleDrawer(anchor, true)}
+                                            PaperProps={{style:{borderTopLeftRadius:'30px',backgroundColor: '#DED8D8',padding:'20px',width:'480px'}}}
+                                        >
+                                            {list(anchor)}
+                                        </SwipeableDrawer>
+                                    </React.Fragment>
+                                ))}
+
+                            </div>
+                        </div>
+                        {/* child box of padding box */}
+                        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '60px'}}>
+                        <img src={grp_img}  alt='grp' style={{ width: '500px',  border: 'solid white 2px', borderRadius: '50px' }} /> 
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '60px'}}>
+                        <Typography> Lorem ipsum dolor sit amet consectetur. Ornare massa nunc nibh tristique. Non ligula tristique ut ut libero sit convallis maecenas. At egestas auctor porta mattis.Lorem ipsum dolor sit amet consectetur. Ornare massa nunc nibh tristique. Non ligula tristique ut ut libero sit convallis maecenas. At egestas auctor porta mattis.
+                        </Typography>
+                        </div>
+                    </div>
+                </div>
+                {/* footer */}
+                <footer style={{ display: 'flex', justifyContent: 'center', backgroundColor: '#C31E2C', width: '100%', marginTop: '70px', paddingTop: '10px' }}>
+                    <div style={{ width: '500px', padding: '50px' }}>
+                        <img src={cafe} alt='cafe' style={{ width: '200px' }} />
+                        <Typography style={{ color: '#DAC6C7' }}>At Quick Cafe our core beliefs drive everything we do. We believe in the power of innovation to revolutionize the way people experience food and dining. We're passionate about harnessing technology to make dining experiences more convenient, efficient, and enjoyable for everyone.</Typography>
+                    </div>
+                    <div style={{ padding: '50px' }}>
+                        <Typography style={{ color: 'white', fontWeight: 'bold', marginBottom: '50px' }}>Quick links</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Browse Foodtime</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Registrations</Typography>
+                    </div>
+                    <div style={{ padding: '50px' }}>
+                        <Typography style={{ color: 'white', fontWeight: 'bold', marginBottom: '50px' }}>About</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>About us</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Mission</Typography>
+                    </div>
+                    <div style={{ padding: '50px' }}>
+                        <Typography style={{ color: 'white', fontWeight: 'bold', marginBottom: '50px' }}>Contact</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Facebook</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Twitter</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Instagram</Typography>
+                    </div>
+                </footer>
+            </div>
+        </ThemeProvider>
     )
 }
 
--- CanteenAutomation/frontEnd/src/custom_compo/contact.js
+++ CanteenAutomation/frontEnd/src/custom_compo/contact.js
@@ -1,8 +1,187 @@
-function Contact(){
-    return(
-        <div>
-            Lorem ipsum
-        </div>
+//importing css
+import '../App.css';
+
+//importing MUI cmp
+
+import { Typography, createTheme } from '@mui/material/';
+import { ThemeProvider } from '@mui/material/';
+import { green } from '@mui/material/colors';
+import Button from '@mui/material/Button';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+
+//importing photos
+import logo from '../general_compo/logo.png'
+import cafe from "../general_compo/cafe.png"
+
+
+//importing custom cmp
+import CartContent from './cartContent.js';
+import AccountContent from './accountContent.js';
+
+//defining theme
+const theme = createTheme({
+    palette: {
+        primary: { main: "#C31E2C" },
+        secondary: green
+    }
+})
+
+function Contact() {
+
+    //state for drawer
+    const [state, setState] = React.useState({
+        right: false,
+        left: false
+    });
+
+    //function for toggling the drawer
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (
+            event &&
+            event.type === 'keydown' &&
+            (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const closeButton = (anchor, status) => {
+        setState({ ...state, [anchor]: status });
+    }
+
+    const drawerButton = (anchor, status) => {
+        closeButton(anchor, status)
+    }
+
+    //list of all the items we need to display in the cart drawer
+    const list = (anchor) => (
+        <Box
+            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+            role="presentation"
+        //onClick={toggleDrawer(anchor, false)}
+        //onKeyDown={toggleDrawer(anchor, false)}
+        >
+            {anchor === "right" ? <CartContent drawerButton={drawerButton} anchor={anchor} /> : <AccountContent drawerButton={drawerButton} anchor={anchor} />}
+        </Box>
+    );
+
+    return (
+        <ThemeProvider theme={theme}>
+            {/* background */}
+            <div style={{ backgroundColor: '#DED8D8', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                {/* first box */}
+                <div style={{ borderRadius: '108px', marginTop: '70px', backgroundColor: '#EBE7E6', border: '2px solid white', width: '1341px', height: '732px', boxShadow: '0px 10px 5px darkgrey', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                    {/* padding box */}
+                    <div style={{ width: '1191px', height: '2032px' }}>
+                        {/* header div / Navigation bar */}
+                        <div style={{ display: 'flex', height: '70px', justifyContent: 'center', marginTop: '70px' }}>
+                            <img src={logo} alt='website logo' style={{ marginRight: '250px', height: '80px' }} />
+                            <div style={{ display: 'flex', boxShadow: '0px 2px 0px darkGrey', paddingBottom: '10px', marginTop: '10px' }}>
+                                <Button style={{ color: 'black', marginRight: '20px', marginTop: '10px', fontWeight: 'bold' }} href='/home'>Home</Button>
+                                <Button style={{ color: 'black', marginRight: '20px', marginTop: '10px', fontWeight: 'bold' }} href='/home/feedback'>Feedback</Button>
+                                <Button style={{ color: 'black', marginRight: '20px', marginTop: '10px', fontWeight: 'bold' }} href='/home/aboutus'>About Us</Button>
+                                <Button style={{ color: 'black', marginRight: '60px', marginTop: '10px', fontWeight: 'bold' }} href='/home/contact'>Contact</Button>
+
+                                {/* drawer for cart */}
+                                {['left'].map((anchor) => (
+                                    <React.Fragment key={anchor}>
+                                        <Button variant='contained' style={{ borderRadius: '30px', marginRight: '20px', marginTop: '10px', fontWeight: 'bold' }} onClick={toggleDrawer(anchor, true)}>Account</Button>
+                                        <SwipeableDrawer
+                                            anchor={anchor}
+                                            open={state[anchor]}
+                                            onClose={toggleDrawer(anchor, false)}
+                                            onOpen={toggleDrawer(anchor, true)}
+                                            PaperProps={{ style: { borderTopRightRadius: '30px', backgroundColor: '#DED8D8', padding: '20px', width: '480px' } }}
+                                        >
+                                            {list(anchor)}
+                                        </SwipeableDrawer>
+                                    </React.Fragment>
+                                ))}
+                                {['right'].map((anchor) => (
+                                    <React.Fragment key={anchor}>
+                                        <Button variant='contained' startIcon={<ShoppingCartIcon />} style={{ borderRadius: '50px', marginRight: '20px', marginTop: '10px', fontWeight: 'bold' }} onClick={toggleDrawer(anchor, true)}>0</Button>
+                                        <SwipeableDrawer
+                                            anchor={anchor}
+                                            open={state[anchor]}
+                                            onClose={toggleDrawer(anchor, false)}
+                                            onOpen={toggleDrawer(anchor, true)}
+                                            PaperProps={{ style: { borderTopLeftRadius: '30px', backgroundColor: '#DED8D8', padding: '20px', width: '480px' } }}
+                                        >
+                                            {list(anchor)}
+                                        </SwipeableDrawer>
+                                    </React.Fragment>
+                                ))}
+
+                            </div>
+                        </div>
+                        {/* child box of padding box */}
+                        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '60px'}}>
+                            <Typography variant='h4' style={{ color: '#C31E2C'}}> Members </Typography>
+                        </div>
+
+                        <div style={{ display: 'flex', justifyContent: 'left', marginTop: '60px', marginLeft: '200px'}}>
+                            <Typography variant='h6'> Thakor Dhruvilsingh </Typography> <Typography variant='h6' style={{ marginLeft: '50px' }}> 202101462 </Typography>
+                            <Typography variant='h6' style={{ marginLeft: '150px'}}> Vrajkumar Patel </Typography> <Typography variant='h6' style={{ marginLeft: '50px' }}> 202101060 </Typography>
+                        </div>
+
+                        <div style={{ display: 'flex', justifyContent: 'left', marginTop: '30px', marginLeft: '200px'}}>
+                            <Typography variant='h6'> Bhuva Vandan </Typography> <Typography variant='h6' style={{ marginLeft: '100px' }}> 202101404 </Typography>
+                            <Typography variant='h6' style={{ marginLeft: '150px' }}> Yash Kodwani </Typography> <Typography variant='h6' style={{ marginLeft: '65px' }}> 202101418 </Typography>
+                        </div>
+
+                        <div style={{ display: 'flex', justifyContent: 'left', marginTop: '30px', marginLeft: '200px'}}>
+                            <Typography variant='h6'> Dweej Pandya </Typography> <Typography variant='h6' style={{ marginLeft: '100px' }}> 202101432 </Typography>
+                            <Typography variant='h6' style={{ marginLeft: '150px' }}> Krishna Choksi </Typography> <Typography variant='h6' style={{ marginLeft: '56px' }}> 202101459 </Typography>
+                        </div>
+
+                        <div style={{ display: 'flex', justifyContent: 'left', marginTop: '30px', marginLeft: '200px'}}>
+                            <Typography variant='h6'> Kalhar Patel </Typography> <Typography variant='h6' style={{ marginLeft: '120px' }}> 202101009 </Typography>
+                            <Typography variant='h6' style={{ marginLeft: '150px' }}> Umang Trivedi </Typography> <Typography variant='h6' style={{ marginLeft: '60px' }}> 202101471 </Typography>
+                        </div>
+
+                        <div style={{ display: 'flex', justifyContent: 'left', marginTop: '30px', marginLeft: '200px'}}>
+                            <Typography variant='h6'> Mihir Goel </Typography> <Typography variant='h6' style={{ marginLeft: '138px' }}> 202101473 </Typography>
+                            <Typography variant='h6' style={{ marginLeft: '150px'}}> Kalathiya Shrut </Typography> <Typography variant='h6' style={{ marginLeft: '53px' }}> 202101479 </Typography>
+                        </div>
+
+                        <div style={{ display: 'flex', justifyContent: 'left', marginTop: '30px', marginLeft: '200px'}}>
+                            <Typography variant='h6'> Khakhariya Shrey </Typography> <Typography variant='h6' style={{ marginLeft: '70px' }}> 202101493 </Typography>
+                            <Typography variant='h6' style={{ marginLeft: '150px' }}> Jay Dobariya </Typography> <Typography variant='h6' style={{ marginLeft: '70px' }}> 202101521 </Typography>
+                        </div>
+                        
+                    </div>
+                </div>
+                {/* footer */}
+                <footer style={{ display: 'flex', justifyContent: 'center', backgroundColor: '#C31E2C', width: '100%', marginTop: '70px', paddingTop: '10px' }}>
+                    <div style={{ width: '500px', padding: '50px' }}>
+                        <img src={cafe} alt='cafe' style={{ width: '200px' }} />
+                        <Typography style={{ color: '#DAC6C7' }}>At Quick Cafe our core beliefs drive everything we do. We believe in the power of innovation to revolutionize the way people experience food and dining. We're passionate about harnessing technology to make dining experiences more convenient, efficient, and enjoyable for everyone.</Typography>
+                    </div>
+                    <div style={{ padding: '50px' }}>
+                        <Typography style={{ color: 'white', fontWeight: 'bold', marginBottom: '50px' }}>Quick links</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Browse Foodtime</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Registrations</Typography>
+                    </div>
+                    <div style={{ padding: '50px' }}>
+                        <Typography style={{ color: 'white', fontWeight: 'bold', marginBottom: '50px' }}>About</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>About us</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Mission</Typography>
+                    </div>
+                    <div style={{ padding: '50px' }}>
+                        <Typography style={{ color: 'white', fontWeight: 'bold', marginBottom: '50px' }}>Contact</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Facebook</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Twitter</Typography>
+                        <Typography style={{ color: '#DAC6C7', marginBottom: '20px' }}>Instagram</Typography>
+                    </div>
+                </footer>
+            </div>
+        </ThemeProvider>
     )
 }
 
